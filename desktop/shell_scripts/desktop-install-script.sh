e372f4156000ceb8bab94afe4ba01ca8ee92fbce
theadam43@gmail.com <theadam43@gmail.com>
2017-03-28 01:40:40 -0600

    Rudimentary cava autostart. Need devilspie.

--- desktop/shell_scripts/desktop-install-script.sh
+++ desktop/shell_scripts/desktop-install-script.sh
@@ -10,11 +10,6 @@ echo "Run as root."
 
 
 
-echo "Connect to wifi or ethernet."
-wifi-menu
-# Pulls up wifi connectivity options.
-
-
 
 ####### USER SETUP
 echo
@@ -41,6 +36,7 @@ echo "Username to add: $NEWUSER"
 
 
 
+
 ########### Append user to Sudoers file
 echo
 echo "Configuring sudo priviliges."
@@ -49,6 +45,7 @@ useradd -m -G wheel -s /bin/bash ${NEWUSER}
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 echo "# Above line appended by Athens install script." >> /etc/sudoers
 # Allows user to use sudo command.
+
 
 
 
@@ -93,6 +90,7 @@ echo "$NEWUSER password assigned."
 
 
 
+
 ######### Hostname Setup
 echo
 echo -n "Enter your new desired computer hostname (default: ellas) > "
@@ -102,6 +100,7 @@ hostnamectl set-hostname ${NEWHOSTNAME}
 echo
 # Ask user for the name of the computer as seen on networks.
 # Default is ellas
+
 
 
 
@@ -131,14 +130,20 @@ echo "# Above code added by Athens install script." >> /etc/locale.conf
 
 
 
+
 ########## SYU
 echo
 echo "-----------------------------------------"
+echo "Connect to wifi or ethernet."
+wifi-menu
+# Pulls up wifi connectivity options.
+echo
 echo "Updating base system."
 echo
 pacman -Syu
 echo
 # Updates/upgrades system.
+
 
 
 
@@ -149,6 +154,7 @@ echo "Installing "GNOME" desktop environment."
 echo
 pacman -S gnome gnome-keyring gnome-software tlp xfce4-terminal gdm
 # Gnome standard packages and TLP battery saver.
+
 
 
 
@@ -167,6 +173,7 @@ pacman -Rs evolution
 ###### Removed this functionality because the default ones look ugly, but I'm keeping the code in just in case.
 ###### Can be undone using the Gnome Tweak Tool
 # Allows Gnome to show desk icons, enables GDM Lock Screen.
+
 
 
 
@@ -194,36 +201,16 @@ systemctl disable systemd-rfkill.service
 
 
 
+
 ######### Programming Tools
 echo
 echo "-----------------------------------------"
 echo "Installing programming tools."
 echo
-pacman -S git base-devel wget gvfs gamin bash-completion tk python3 python2 gparted dosfstools conky atom
-touch /home/${NEWUSER}/.xprofile
-cp /../config_files/conky-visor.txt /home/${NEWUSER}/.config/conky/convisor.conf
-cp /../config_files/conky-tasks.txt /home/${NEWUSER}/.config/conky/contasks.conf
-touch /home/${NEWUSER}/tasks.txt
-echo "Add tasks in ~/tasks.txt"
-echo "conky" >> /home/${NEWUSER}/.xprofile
-echo "# Above comment appended by Athens install script." >> /home/${NEWUSER}/.xprofile
-# Copies all relevant things into conky.conf, conky autostarts at system login
+pacman -S git base-devel wget gvfs gamin bash-completion tk python3 python2 gparted dosfstools conky atom tilda
 git config --global core.editor nano
 # Makes git's default editor as nano, not emacs.
 # Installs basic programming tools.
-
-
-####### Install Atom packages in $NEWUSER home directory
-su $NEWUSER
-apm install minimap git-diff highlight-line symbols-tree-view script
-# minimap, git differentiation, highlights the current line you're on, show symbols in tree view, run scripts.
-apm install linter linter-ui-default  busy-signals intentions tablr atom-save-all
-# General base for installing syntax highlighters, tablr for CSV/TSV, save all windows at once.
-apm install linter-pylama magicpython
-# python syntax-highlighting
-apm install open-in-browser open-in-browsers linter-tidy
-exit
-# Installs desired Atom packages.
 
 
 
@@ -240,6 +227,7 @@ pacman -S transmission-cli transmission-gtk chromium
 
 
 
+
 ########### Configuring Network Tools
 echo
 echo "Configuring network tools."
@@ -250,12 +238,14 @@ gpasswd -a ${NEWUSER} network
 
 
 
+
 ########## File Processing Tools
 echo
 echo "-----------------------------------------"
 echo "Installing file processing tools."
 echo
 pacman -S gimp libreoffice-fresh
+
 
 
 
@@ -273,13 +263,67 @@ systemctl enable bluetooth.service
 
 
 
+
+####### Install Atom packages in $NEWUSER home directory
+su $NEWUSER
+apm install minimap git-diff highlight-line symbols-tree-view script
+# minimap, git differentiation, highlights the current line you're on, show symbols in tree view, run scripts.
+apm install linter linter-ui-default  busy-signals intentions tablr atom-save-all
+# General base for installing syntax highlighters, tablr for CSV/TSV, save all windows at once.
+apm install linter-pylama magicpython
+# python syntax-highlighting
+apm install open-in-browser open-in-browsers linter-tidy
+exit
+# Installs desired Atom packages.
+
+
+
+
+####### Run AUR Install script.
+cp aur-desktop.sh /home/$NEWUSER
+su $NEWUSER
+cd
+./aur-desktop.sh
+exit
+# Put a copy of the aur script in the user home folder and run it as user.
+
+
+
+
+######### Configure .xprofile to run aesthetics programs at boot.
+touch /home/${NEWUSER}/.xprofile
+# Create .xprofile
+cp /../config_files/conky-visor.txt /home/${NEWUSER}/.config/conky/convisor.conf
+cp /../config_files/conky-tasks.txt /home/${NEWUSER}/.config/conky/contasks.conf
+# Copy the contents of the conky shell scripts to config files.
+touch /home/${NEWUSER}/tasks.txt
+echo "Add tasks in ~/tasks.txt" >> /home/$NEWUSER/tasks.txt
+# Create todo list file for contasks.conf, will not display without this.
+echo "conky -c ~/.config/conky/contasks.conf" >> /home/${NEWUSER}/.xprofile
+# Run todo list displaying from ~/tasks.txt
+echo "conky -c ~/.config/conky/convisor.conf" >> /home/${NEWUSER}/.xprofile
+# Run system monitor at boot
+cp /../config_files/cava-config.txt /home/${NEWUSER}/.config/cava/config
+# Copy cava config file from here to requisite folder
+cp /../config_files/tilda-config.txt /home/${NEWUSER}/.config/tilda/config_0
+echo "tilda" >> /home/${NEWUSER}/.xprofile
+echo "# Run tilda -C to configure options" >> /home/${NEWUSER}/.xprofile
+# Create tilda config file for visualizing audio with cava
+# Tilda will run at boot, and it will run cava
+echo "# Above comments appended by Athens install script." >> /home/${NEWUSER}/.xprofile
+# Copies all relevant things into conky.conf, conky autostarts at system login
+
+
+
+
 ########## Final Comment
 echo
 echo "-----------------------------------------"
 echo "Auto-installed system tools."
 echo "Full desktop has been configured."
+echo "AUR packages have been installed."
 echo "A restart is necessary."
-echo "To install AUR packages or an AUR helper, see the AUR script."
 echo "-----------------------------------------"
 echo
 echo "Arch Linux is the best!"
+echo
